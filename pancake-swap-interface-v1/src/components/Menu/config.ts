4fef26fc43e5be796fd10c841b5a65e2a3b36921
TLan <thelan@TLan-MacAir.local>
2022-04-29 09:45:23 +0700

    first commit

---
+++ pancake-swap-interface-v1/src/components/Menu/config.ts
@@ -0,0 +1,23 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool',
+      },
+      
+    ],
+  },
+]
+
+export default config
